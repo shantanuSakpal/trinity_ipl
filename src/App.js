645e5fb1592f4a9c3c412cf8c35f93b8ef5f4414
Shantanu Sakpal <shantanuesakpal1420@gmail.com>
2023-02-16 10:39:39 +0530

    navbar added links set

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { ErrorPage } from './pages/ErrorPage';
 import { Navbar } from './components/Navbar';
 import { Footer } from './components/Footer';
+import { About } from './pages/About';
+import { Services } from './pages/Services';
+import { Pricing } from './pages/Pricing';
+import { Contact } from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
+    <div className="p-2">
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="*" element={<ErrorPage />}></Route>
-        </Routes>
+        <Navbar />
+        <div className='min-h-screen'>
+          <Routes>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/services" element={<Services />}></Route>
+            <Route path="/pricing" element={<Pricing />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="*" element={<ErrorPage />}></Route>
+          </Routes>
+        </div>
+        <Footer />
       </Router>
-      <Footer />
+
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Footer = () => {
     return (
-        <div className='bottom-0' >Copyright 2023</div>
+        <div className='bottom-0 text-center bbg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 text-gray-400' >Copyright Coding Bots</div>
     )
 }
